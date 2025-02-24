1ec9a1660df85a921dda9157a846e036af749c89
Mati Joseph <crucialniccur@gmail.com>
2025-02-24 22:32:21 +0300

    define a default value for the second parameter and test the function using console.log

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function introductionWithLanguage(name, language) {
   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-function introductionWithLanguageOptional(name, language) {
+function introductionWithLanguageOptional(name, language = "JavaScript") {
   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
+
+console.log(introductionWithLanguageOptional("mati"));
